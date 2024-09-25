0f801c2f5f1fb29359a9b91617a5c5c25caa1ac3
titaniaaurelliaa <titaniaaurelliaputri@gmail.com>
2024-09-25 12:10:12 +0700

    JS-05 prak 5.2

--- JS - 05 PHP part 2/recursive.php
+++ JS - 05 PHP part 2/recursive.php
@@ -1,9 +1,21 @@
 <?php
-function tampilkanHaloDunia(){
-    echo "Halo dunia! <br/>";
+//5.1
+// function tampilkanHaloDunia(){
+//     echo "Halo dunia! <br/>";
 
-    tampilkanHaloDunia();
+//     tampilkanHaloDunia();
+// }
+
+// tampilkanHaloDunia();
+
+//5.2
+function tampilkanAngka(int $jumlah, int $indeks = 1){
+    echo "Perulangan ke-{$indeks} <br>";
+
+    //panggil diri sendiri selama $indeks <= jumlah
+    if ($indeks < $jumlah){
+        tampilkanAngka($jumlah, $indeks + 1);
+    }
 }
-
-tampilkanHaloDunia();
+tampilkanAngka(20);
 ?>
