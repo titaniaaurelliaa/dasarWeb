7915deafbab2a165a2e747b53d72ce97500f778a
titaniaaurelliaa <titaniaaurelliaputri@gmail.com>
2024-09-25 09:07:13 +0700

    JS-05 prak 1

---
+++ JS - 05 PHP part 2/array_1.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+
+    </head>
+    <body>
+        <h2>Array Terindeks</h2>
+        <?php
+        $Listdosen = ["Elok Nur Hamdana", "Unggul Pamenang", "Bagas Nugraha"];
+
+        echo $Listdosen[2] . "<br>";
+        echo $Listdosen[0] . "<br>";
+        echo $Listdosen[1] . "<br>";
+
+        echo "<h2>Menggunakan Loop</h2>";
+        for ($i = 0; $i < count($Listdosen); $i++) {
+            echo $Listdosen[$i] . "<br>";
+        }
+        ?>
+    </body>
+</html>
