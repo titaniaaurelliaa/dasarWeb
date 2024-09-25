5c4e6b02fe75460d24faf7adcdd3f5deceed9fcb
titaniaaurelliaa <titaniaaurelliaputri@gmail.com>
2024-09-25 10:42:03 +0700

    JS-05 prak 4.2

--- JS - 05 PHP part 2/function.php
+++ JS - 05 PHP part 2/function.php
@@ -1,10 +1,28 @@
 <?php
-function perkenalan(){
-    echo "Assalamualaikum, ";
-    echo "Perkenalkan, nama saya Titania <br/>";
+//4.1
+// function perkenalan(){
+//     echo "Assalamualaikum, ";
+//     echo "Perkenalkan, nama saya Titania <br/>";
+//     echo "Senang berkenalan dengan Anda <br/>";
+// }
+// //memanggil fungsi
+// perkenalan();
+// perkenalan();
+
+//4.2
+function perkenalan($nama, $salam){
+    echo $salam.", ";
+    echo "Perkenalkan, nama saya ".$nama." <br/>";
     echo "Senang berkenalan dengan Anda <br/>";
 }
-// memanggil fungsi
-perkenalan();
-perkenalan();
+
+//memanggil fungsi
+perkenalan("Titania", "Haii");
+echo "<br>";
+
+$saya = "Titann";
+$ucapanSalam = "Morningg";
+
+//memanggil lagi
+perkenalan($saya, $ucapanSalam);
 ?>
