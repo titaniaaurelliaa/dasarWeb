0a2a3d44ffdaa0a53a826ca12b005d68c939c6b4
titaniaaurelliaa <titaniaaurelliaputri@gmail.com>
2024-10-09 14:03:02 +0700

    JS-07 prak 5.3

--- JS - 07 Form Processing/regex.php
+++ JS - 07 Form Processing/regex.php
@@ -10,8 +10,14 @@ if (preg_match($pattern, $text)){
 $pattern = '/[0-9]+/'; //cocokkan satu atau lebih digit.
 $text = 'There are 123 apples.';
 if (preg_match($pattern, $text, $matches)){
-    echo "Cocokkan : " . $matches[0];
+    echo "Cocokkan : " . $matches[0] . "<br>";
 } else {
     echo "Tidak ada yang cocok!<br>";
 }
+
+$pattern = '/apple/';
+$replacment = 'banana';
+$text = 'I like apple pie.';
+$new_text = preg_replace($pattern, $replacment, $text);
+echo $new_text; //output : "I like banana pie."
 ?>
