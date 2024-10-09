4ec85827ee29b83d08067dfef0b0abcdf0a27628
titaniaaurelliaa <titaniaaurelliaputri@gmail.com>
2024-10-09 08:30:06 +0700

    JS-07 prak 2.2

--- JS - 07 Form Processing/empty.php
+++ JS - 07 Form Processing/empty.php
@@ -1,8 +1,14 @@
 <?php
 $myArray = array(); //array kosong
 if (empty($myArray)){
-    echo "Array tidak terdefinisikan atau kosong.";
+    echo "Array tidak terdefinisikan atau kosong.<br>";
 } else {
-    echo "Array terdefinisikan dan tidak kosong.";
+    echo "Array terdefinisikan dan tidak kosong.<br>";
+}
+
+if (empty($nonExistentVar)){
+    echo "Variabel tidak terdefinisikan atau kosong.";
+} else {
+    echo "Variabel terdefinisi dan tidak kosong.";
 }
 ?>
