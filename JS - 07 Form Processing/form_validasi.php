bf585272fed3a333db918b438dae16e5eb1d863f
titaniaaurelliaa <titaniaaurelliaputri@gmail.com>
2024-10-09 15:18:09 +0700

    JS-07 prak 7.2

--- JS - 07 Form Processing/form_validasi.php
+++ JS - 07 Form Processing/form_validasi.php
@@ -1,19 +1,52 @@
 <html>
     <head>
         <title>Form input dengan validasi</title>
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     </head>
     <body>
         <h1>Form input dengan validasi</h1>
         <form method="post" action="proses_validasi.php">
             <label for="nama">Nama : </label>
             <input type="text" id="nama" name="nama">
+            <span id="nama-error" style="color : red;"></span>
             <br>
 
             <label for="email">Email : </label>
             <input type="text" id="email" name="email">
+            <span id="email-error" style="color : red;"></span>
             <br>
 
             <input type="submit" value="submit">
         </form>
+
+        <script>
+            $(document).ready(function() {
+                $("#myForm").submit(function(event) {
+                    var nama = $("#nama").val();
+                    var email = $("#email").val();
+                    var valid = true;
+
+                    if (nama === "") {
+                        $("#nama-error").text("Nama harus diisi.");
+                        valid = false;
+                    } else {
+                        $("#nama-error").text("");
+                    }
+
+                    if (email === "") {
+                        $("#email-error").text("Email harus diisi.");
+                        valid = false;
+                    } else {
+                        $("#email-error").text("");
+                    }
+
+                    if (valid) {
+                        (!event.preventDefault()
+                        // menghentikan pengiriman form jika validasi gagal
+                    )
+                    }
+                });
+            });
+        </script>
     </body>
 </html>
