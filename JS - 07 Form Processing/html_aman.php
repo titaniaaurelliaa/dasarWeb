d06ef147a0e892589eb727accd9f245584e29e3c
titaniaaurelliaa <titaniaaurelliaputri@gmail.com>
2024-10-09 13:38:06 +0700

    JS-07 prak 4.2

--- JS - 07 Form Processing/html_aman.php
+++ JS - 07 Form Processing/html_aman.php
@@ -10,6 +10,10 @@
         <form action="html_aman.php" method="post">
             <label for="input">Masukkan data :</label><br>
             <input type="text" id="input" name="input"><br><br>
+
+            <label for="email">Masukkan email :</label><br>
+            <input type="text" id="email" name="email"><br><br>
+
             <input type="submit" value="Submit">
         </form>
 
@@ -20,11 +24,21 @@
             //sanitasi input untuk mencegah XSS
             $input = htmlspecialchars($input, ENT_QUOTES, 'UTF-8');
 
-            //validasi sederhana: pastikan input tidak kosong
+            //validasi sederhana : pastikan input tidak kosong
             if (!empty($input)) {
                 echo "<p>Data yang dimasukkan : " . $input . "</p>";
             } else {
                 echo "<p>Input tidak boleh kosong!</p>";
+            }
+
+            //memeriksa apakah input adalah email yang valid
+            $email = $_POST['email'];
+            if (filter_var($email, FILTER_VALIDATE_EMAIL)){
+                //lanjutkan dengan pengolahan email yang aman
+                echo "<p>Email yang dimasukkan valid: " . htmlspecialchars($email, ENT_QUOTES, 'UTF-8') . "</p>";
+            } else {
+                //tangani input yang tidak valid
+                echo "<p>Email tidak valid, silakan masukkan email yang benar.</p>";
             }
         }
         ?>
