5e3f944da955789763a801e839b936df21678b9c
titaniaaurelliaa <titaniaaurelliaputri@gmail.com>
2024-10-09 15:02:27 +0700

    JS-07 prak 7.1

---
+++ JS - 07 Form Processing/form_validasi.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <title>Form input dengan validasi</title>
+    </head>
+    <body>
+        <h1>Form input dengan validasi</h1>
+        <form method="post" action="proses_validasi.php">
+            <label for="nama">Nama : </label>
+            <input type="text" id="nama" name="nama">
+            <br>
+
+            <label for="email">Email : </label>
+            <input type="text" id="email" name="email">
+            <br>
+
+            <input type="submit" value="submit">
+        </form>
+    </body>
+</html>
