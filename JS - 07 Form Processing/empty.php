eba5ddc10aef1d89360f17999987f3a608413fa3
titaniaaurelliaa <titaniaaurelliaputri@gmail.com>
2024-10-09 08:22:48 +0700

    JS-07 prak 2.1

---
+++ JS - 07 Form Processing/empty.php
@@ -0,0 +1,8 @@
+<?php
+$myArray = array(); //array kosong
+if (empty($myArray)){
+    echo "Array tidak terdefinisikan atau kosong.";
+} else {
+    echo "Array terdefinisikan dan tidak kosong.";
+}
+?>
