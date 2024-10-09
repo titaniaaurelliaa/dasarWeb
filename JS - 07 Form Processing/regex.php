6cc0b7bba1bb7da80e2c92fff66929f56e24cd9c
titaniaaurelliaa <titaniaaurelliaputri@gmail.com>
2024-10-09 13:55:08 +0700

    JS-07 prak 5.2

--- JS - 07 Form Processing/regex.php
+++ JS - 07 Form Processing/regex.php
@@ -2,8 +2,16 @@
 $pattern = '/[a-z]/'; //cocokkan huruf kecil
 $text = 'This is a Sample Text.';
 if (preg_match($pattern, $text)){
-    echo "Huruf kecil ditemukan!";
+    echo "Huruf kecil ditemukan!<br>";
 } else {
-    echo "Tidak ada huruf kecil!";
+    echo "Tidak ada huruf kecil!<br>";
+}
+
+$pattern = '/[0-9]+/'; //cocokkan satu atau lebih digit.
+$text = 'There are 123 apples.';
+if (preg_match($pattern, $text, $matches)){
+    echo "Cocokkan : " . $matches[0];
+} else {
+    echo "Tidak ada yang cocok!<br>";
 }
 ?>
