e93c0eaf9b5a97d9d90f57a840ad5f855fcd5242
titaniaaurelliaa <titaniaaurelliaputri@gmail.com>
2024-10-16 15:41:09 +0700

    JS-08 prak 8

---
+++ JS - 08 PHP From Upload/sessionCall.php
@@ -0,0 +1,12 @@
+<?php
+    session_start();
+?>
+
+<html>
+    <body>
+        <?php
+            echo "Favorite color is " . $_SESSION["favcolor"] . ".<br>";
+            echo "Favorite animal is " . $_SESSION["favanimal"] . ".<br>";
+        ?>
+    </body>
+</html>
