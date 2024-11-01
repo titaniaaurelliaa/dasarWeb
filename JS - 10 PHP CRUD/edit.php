81a3a2b8fdb7dd01a8f80f54207426f11bccf5bd
titaniaaurelliaa <titaniaaurelliaputri@gmail.com>
2024-11-01 15:40:09 +0700

    JS-10.2 prak 5

--- JS - 10 PHP CRUD/edit.php
+++ JS - 10 PHP CRUD/edit.php
@@ -1,7 +1,7 @@
 <html>
     <head>
         <title>Edit Data Anggota</title>
-        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     </head>
     <body>
         <?php
@@ -12,28 +12,43 @@
         $row = mysqli_fetch_assoc($result);
         mysqli_close($koneksi);
         ?>
-        <div class="container">
+        <div class="container mt-4">
             <h2>Edit Data Anggota</h2>
             <form action="proses.php?aksi=ubah" method="post">
                 <input type="hidden" name="id" value="<?php echo $row['id']; ?>">
-                <label for="nama">Nama : </label>
-                <input type="text" name="nama" id="nama" value="<?php echo $row['nama']; ?>" required>
-                <label for="jenis_kelamin">Jenis Kelamin : </label>
-                <div class="radio-group">
-                    <input type="radio" name="jenis_kelamin" value="L" id="laki" 
-                        <? if ($row['jenis_kelamin'] === 'L') echo 'checked'; ?> required>
-                    <label for="laki">Laki-laki</label>
-                    <input type="radio" name="jenis_kelamin" value="P" id="perempuan" 
-                        <? if ($row['jenis_kelamin'] === 'P') echo 'checked'; ?> required>
-                    <label for="perempuan">Perempuan</label>
+                <div class="from-group">
+                    <label for="nama">Nama : </label>
+                    <input type="text" class="form-control" name="nama" id="nama" value="<?php echo $row['nama']; ?>" required>
                 </div>
-                <label for="alamat">Alamat : </label>
-                <input type="text" name="alamat" id="alamat" value="<?php echo $row['alamat']; ?>" required>
-                <label for="no_telp">No. Telp : </label>
-                <input type="text" name="no_telp" id="no_telp" value="<?php echo $row['no_telp']; ?>" required>
-                <button type="submit">Simpan Perubahan</button>
-                <a href="index.php" class="btn-kembali">Kembali</a>
-            </form>
+                <div class="from-group">
+                    <label for="jenis_kelamin">Jenis Kelamin : </label>
+                    <div class="from-check">
+                        <input type="radio" class="from-check-input" name="jenis_kelamin" value="L" id="laki" 
+                            <? if ($row['jenis_kelamin'] === 'L') echo 'checked'; ?>required>
+                        <label class="from-check-input" for="laki">Laki-laki</label>
+                    </div>
+                    <div class="from-check">
+                        <input type="radio" class="from-check-input" name="jenis_kelamin" value="P" id="perempuan" 
+                            <? if ($row['jenis_kelamin'] === 'P') echo 'checked'; ?>required>
+                        <label class="from-check-input" for="perempuan">Perempuan</label>
+                    </div>
+                </div>
+                <div class="from-group">
+                    <label for="alamat">Alamat : </label>
+                    <input type="text" class="form-control" name="alamat" id="alamat" value="<?php echo $row['alamat']; ?>" required>
+                </div>
+                <div class="from-group">
+                    <label for="no_telp">No. Telp : </label>
+                    <input type="text" class="form-control" name="no_telp" id="no_telp" value="<?php echo $row['no_telp']; ?>" required>
+                </div>
+                <button type="submit" class="btn btn-primary">Simpan Perubahan</button>
+            </from>
+            
+            <a class="btn btn-secondary mt-2" href="index.php">Kembali</a>
         </div>
+
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     </body>
 </html>
