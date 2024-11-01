90a3f00cf985af818c6c4e5cdeb28a951cdca9c4
titaniaaurelliaa <titaniaaurelliaputri@gmail.com>
2024-11-01 14:19:09 +0700

    JS-10.2 prak 4

--- JS - 10 PHP CRUD/proses.php
+++ JS - 10 PHP CRUD/proses.php
@@ -37,6 +37,23 @@ if ($aksi == 'tambah') {
     } else {
         echo "ID tidak valid.";
     }
+} elseif ($aksi == 'hapus') {
+    if (isset($_GET['id'])) {
+        $id = $_GET['id'];
+
+        $query = "DELETE FROM anggota WHERE id=$id";
+
+        if (mysqli_query($koneksi, $query)) {
+            header("Location: index.php");
+            exit();
+        } else {
+            echo "Gagal menghapus data : " . mysqli_error($koneksi);
+        }
+    } else {
+        echo "ID tidak valid.";
+    }
+} else {
+    header("Location: index.php");
 }
 
 mysqli_close($koneksi);
