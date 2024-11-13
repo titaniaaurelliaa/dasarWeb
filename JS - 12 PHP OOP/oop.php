15ad171dd351bc0a8013d61e54dde7904dc9ee7a
titaniaaurelliaa <titaniaaurelliaputri@gmail.com>
2024-11-13 12:19:21 +0700

    JS-12 prak 1.7

--- JS - 12 PHP OOP/oop.php
+++ JS - 12 PHP OOP/oop.php
@@ -15,4 +15,24 @@
 
 // $car1->startEngine();
 // echo $car2->brand;
+
+class Car {
+    private $brand;
+
+    public function __construct($brand) {
+        echo "A new car is created.<br>";
+        $this->brand = $brand;
+    }
+
+    public function getBrand() {
+        return $this->brand;
+    }
+
+    public function __destruct() {
+        echo "The car is destroyed.<br>";
+    }
+}
+
+$car = new Car("Toyota");
+echo "Brand : " . $car->getBrand() . "<br>";
 ?>
